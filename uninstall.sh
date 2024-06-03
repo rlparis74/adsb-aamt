3d9e23123978cbe6275ad2bdd8d9af70583390a2
rlparis74 <40063388+rlparis74@users.noreply.github.com>
2024-06-03 12:03:56 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -x
 
-IPATH=/usr/local/share/adsbfi
+IPATH=/usr/local/share/adsb-aamt
 
-systemctl disable --now adsbfi-mlat
-systemctl disable --now adsbfi-mlat2 &>/dev/null
-systemctl disable --now adsbfi-feed
+systemctl disable --now adsb-aamt-mlat
+systemctl disable --now adsb-aamt-mlat2 &>/dev/null
+systemctl disable --now adsb-aamt-feed
 
 if [[ -d /usr/local/share/tar1090/html-adsbfi ]]; then
     bash /usr/local/share/tar1090/uninstall.sh adsbfi
 fi
 
-rm -f /lib/systemd/system/adsbfi-mlat.service
-rm -f /lib/systemd/system/adsbfi-mlat2.service
-rm -f /lib/systemd/system/adsbfi-feed.service
+rm -f /lib/systemd/system/adsb-aamt-mlat.service
+rm -f /lib/systemd/system/adsb-aamt-mlat2.service
+rm -f /lib/systemd/system/adsb-aamt-feed.service
 
-cp -f "$IPATH/adsbfi-uuid" /tmp/adsbfi-uuid
+cp -f "$IPATH/adsb-aamt-uuid" /tmp/adsb-aamt-uuid
 rm -rf "$IPATH"
 mkdir -p "$IPATH"
-mv -f /tmp/adsbfi-uuid "$IPATH/adsbfi-uuid"
+mv -f /tmp/adsbfi-uuid "$IPATH/adsb-aamt-uuid"
 
 set +x
 
