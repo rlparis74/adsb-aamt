5c24083a253cf24de3ded4d282012191d8d51b18
rlparis74 <40063388+rlparis74@users.noreply.github.com>
2024-06-03 11:54:27 -0400

    Update and rename adsbfi-feed.sh to adsb-aamt-feed.sh

--- scripts/adsb-aamt-feed.sh
+++ scripts/adsb-aamt-feed.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if grep -qs -e 'LATITUDE' /boot/adsb-config.txt &>/dev/null && [[ -f /boot/adsbfi-env ]]; then
+if grep -qs -e 'LATITUDE' /boot/adsb-config.txt &>/dev/null && [[ -f /boot/adsb-aamt-env ]]; then
     source /boot/adsb-config.txt
-    source /boot/adsbfi-env
+    source /boot/adsb-aamt-env
 else
-    source /etc/default/adsbfi
+    source /etc/default/
 fi
 
-if ! [[ -d /run/adsbfi-feed/ ]]; then
-    mkdir -p /run/adsbfi-feed
+if ! [[ -d /run/adsb-aamt-feed/ ]]; then
+    mkdir -p /run/adsb-aamt-feed
 fi
 
 if [[ -z $INPUT ]]; then
@@ -28,8 +28,8 @@ UAT_PORT=$(echo $UAT_INPUT | cut -d: -f2)
 UAT_SOURCE="--net-connector $UAT_IP,$UAT_PORT,uat_in,silent_fail"
 
 
-exec /usr/local/share/adsbfi/feed-adsbfi --net --net-only --quiet \
-    --write-json /run/adsbfi-feed \
+exec /usr/local/share/adsb-aamt/feed-adsb-aamt --net --net-only --quiet \
+    --write-json /run/adsb-aamt-feed \
     --net-beast-reduce-interval $REDUCE_INTERVAL \
     $TARGET $NET_OPTIONS \
     --lat "$LATITUDE" --lon "$LONGITUDE" \
